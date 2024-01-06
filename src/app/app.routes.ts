61191bb936c3d1aacb94eb3d00aead8e413b28a5
Daniel Hernández <manguito_dev@outlook.com>
2024-01-06 00:52:02 -0400

    Auto-generated commit [ci skip]

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { ScheduleComponent } from './pages/schedule/schedule.component';
-
-export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'schedule', component: ScheduleComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' },
-];
